6f978f75180e5a7e926fa46b0e391f1e4975a7f0
Myuddin khatri <khatrimayu111@gmail.com>
2024-01-29 18:35:09 +0530

    fix: reload post comment in patch

--- frappe_social/patches/link_post_in_post_comment.py
+++ frappe_social/patches/link_post_in_post_comment.py
@@ -2,6 +2,7 @@ import frappe
 
 
 def execute():
+	frappe.reload_doc("frappe_social", "doctype", "post_comment")
 	comments = frappe.get_all("Post Comment", ["name", "parent"])
 	for comment in comments:
 		frappe.db.set_value("Post Comment", comment.name, "post", comment.parent)
